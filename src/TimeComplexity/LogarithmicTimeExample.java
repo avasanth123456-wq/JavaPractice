b71643e3712d9a6b7762996a0b331d0587a1a661
avasanth123456-wq <avasanth123456@gmail.com>
2026-02-26 20:06:48 +0530

    Updated time complexity examples

--- src/TimeComplexity/LogarithmicTimeExample.java
+++ src/TimeComplexity/LogarithmicTimeExample.java
@@ -1,4 +1,63 @@
 package TimeComplexity;
+//O(log n)
+/*public class LogarithmicTimeExample {
 
+    public static int binarySearch(int[] arr, int key) {
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] == key)
+                return mid;
+            else if (arr[mid] < key)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50, 60, 70};
+        System.out.println(binarySearch(arr, 50));
+    }
+}*/
 public class LogarithmicTimeExample {
+
+    public static int binarySearch(int[] arr, int target) {
+
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == target)
+                return mid;
+
+            else if (arr[mid] < target)
+                low = mid + 1;
+
+            else
+                high = mid - 1;
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        int[] numbers = {10, 20, 30, 40, 50, 60, 70, 80, 90};
+
+        int result = binarySearch(numbers, 70);
+
+        if (result != -1)
+            System.out.println("Element found at index: " + result);
+        else
+            System.out.println("Element not found");
+    }
 }
+
