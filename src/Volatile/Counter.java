28adefd3f9a5c492fafe730beade149f8b46d47b
avasanth123456-wq <avasanth123456@gmail.com>
2026-08-24 16:37:46 +0530

    Update project files

--- src/Volatile/Counter.java
+++ src/Volatile/Counter.java
@@ -2,23 +2,25 @@ package Volatile;
 
 public class Counter {
 
-    private volatile int count =0;
+    private volatile int count = 0;
 
-    public boolean inc(){
-        if(this.count ==10){
+    public boolean inc() {
+        if (this.count == 10) {
             return false;
         }
         this.count++;
         return true;
     }
 
-    static void main() {
+    public static void main(String[] args) {
+
         Counter counter = new Counter();
 
-        while(counter.inc()){
-            System.out.println("Counter increased");
+        while (counter.inc()) {
+            System.out.println("Counter incremented");
         }
 
         System.out.println("Reached limit 10");
     }
+
 }
